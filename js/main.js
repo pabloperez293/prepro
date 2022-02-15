530d074e7b6704541dd2d33bfd31f3aa9885305a
pabloperez293 <56325243+pabloperez293@users.noreply.github.com>
2022-02-15 06:11:14 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,4 +11,23 @@ $(document).ready(function(){
         $("body").removeClass("hidden");    
 }
 
+// formulario
+    const $form =  document.querySelector('#form')
 
+    $form.addEventListener('submit', handleSubmit)
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const form = new FormData(this)
+        const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+        'Accept' : 'application/json'
+        }
+        })
+        if (response.ok){
+            this.reset()
+            alert("Gracias por contactarme, te estare respondiendo pronto");
+        }
+    }
