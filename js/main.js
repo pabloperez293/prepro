9cfb622ab9c8e226da8613a0135c084f394b5b27
Pablo Perez <56325243+pabloperez293@users.noreply.github.com>
2022-09-15 12:30:10 -0300

    Errror de push en el git

--- js/main.js
+++ js/main.js
@@ -11,4 +11,14 @@ $(document).ready(function(){
         $("body").removeClass("hidden");    
 }
 
+// button up scroll
+document.getElementById("up").addEventListener("click", scrollUp)
 
+function scrollUp() {
+    let scroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if(scroll > 0 ){
+        window.scrollTo (0,0)
+    }
+
+}
